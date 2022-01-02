d02a24c27156d419b252807ac04f8a1016da9895
kreativszoveg <96833164+kreativszoveg@users.noreply.github.com>
2022-01-02 17:16:26 +0100

    Add files via upload

---
+++ scriptek.js
@@ -0,0 +1,65 @@
+  var melleknev; 
+  var fonev;
+  var melleknev2; 
+  var fonev2;
+  var ige; 
+  var multIge; 
+  var faszsag; 
+
+  function ertekadas() {
+    melleknev = document.getElementById("mnev1").value;
+    fonev = document.getElementById("fn1").value;
+    melleknev2 = document.getElementById("mnev2").value;
+    fonev2 = document.getElementById("fn2").value;
+    ige  = document.getElementById("ige1").value;
+    multIge = document.getElementById("ige2").value;
+    faszsag = document.getElementById("faszsag").value;
+
+    letsgo();
+  }
+
+
+
+    
+  function letsgo () {
+
+        var text = [`${fonev} egy ${fonev2} áldozata lett. Senki se tudja, hogy miért nem inkább egy ${melleknev} intézte volna el. A ${melleknev2} szerint ${fonev} szívesen ${multIge}. Azt viszont nem tudni, hogy a ${fonev2}-al/el mi a ${faszsag} történt. Az ügy még mindig ${ige}.`,
+
+        `A ${fonev} -t sikeresen ${multIge} a ${fonev2}. Valószínűleg ennek köze volt a ${melleknev}-hez/hoz és a(z) ${melleknev2}-hez/hoz. Sajnos ezután mindenki ${ige} és ${faszsag}-t hirdetnek.`,
+
+        `${fonev} se tudta, hogy mit ${ige}. Ez egy ${melleknev} este történt. Még a ${fonev2}-nak/nek se volt fogalma semmiről, csak azt ${multIge}, hogy árad a sok ${faszsag}. Másnap mindenki ${melleknev2} lett.`,
+
+        `${fonev} ${multIge}. A kunyhóból kijőve ${ige} egy ${fonev2}-t ami nagyon ${melleknev} lett. ${fonev} szerint ez a pillanat nagyon ${melleknev2} volt és ${faszsag}.`,
+
+    `${fonev2} ajándékba kapott egy ${melleknev}-t. Ezután elkezdte ezt ${ige}-ni. Ezután ${multIge} ${fonev} egy ${melleknev2} ${faszsag}-t.`,
+
+    `Akármennyi ${faszsag} van az utcán, ${fonev} mindig ${ige}-ja őket. Ez mind a polgármesternek: ${fonev2}-nek köszönhető. Ő mindig is egy olyan ${melleknev} volt, aki ${multIge} minden ${melleknev2}-t.`,
+
+    `Ne ítélj a név alapján! Egy ${fonev} is lehet ${fonev2}! Debrőczky Péter, alias ${faszsag} is csak a pénz miatt ${ige}! Ez nem ${melleknev} dolog, ez egyszerűen ${melleknev2}! ${faszsag} tehát kiskorában mindig ${multIge}.`,
+
+    `Az egyik staff titokban ${fonev}, csak nem ${ige}. ${fonev2} már rájött a turpisságra, ezért a következő dolgot tette: ${multIge}. És még azt mondják, hogy itt nincs ${melleknev} és ${melleknev2}. Szerintem ez az egész helyzet egy nagy ${faszsag}.`,
+
+    `Nincs annál jobb, mint amikor ${fonev} ${ige}. Ezt még ${fonev2} is jó szemmel nézi. Természetesen mindkettőjük ${melleknev}, még a ${melleknev2} staff is megmondja ezt. Ezután fogták ${fonev2}-t és ${fonev}-t, majd ${multIge} a staff. Természetesen a végeredmény ${faszsag} lett. `,
+
+    `Meleg van? Nos erről csak a ${faszsag} tehet. Flare már ${multIge} a ${faszsag}-t, hogy a melegség elmúljon, de nem sikerült. ${fonev} előállt egy ötlettel, miszerint ha egy ${melleknev}-t és egy ${melleknev2}-t összeugrasztunk, akkor ${fonev2} alkotódik meg belőle, aki minden nap ${ige} majd.`,
+    
+    `${multIge} majd azt mondta ${fonev}: "${fonev2} egy ${faszsag}". Ezután egy ${melleknev} betörte az ajtót, és a következőt kiáltotta a ${melleknev2} ${fonev}-nak/nek: "Mégis hogyan ${ige} Ön??"`,
+  
+    `A mai napon ${fonev} ${multIge} egy ${melleknev} partyban. Az egyik ${fonev2} bekapta szegény ${fonev} ${faszsag}-át/ét. Amikor hazaért, már rohadt ${melleknev2} volt, és elhatározta, hogy ${ige} egyet.`,
+    
+    `Tegnap mikor éppen ${fonev}-t sétáltattam, odajött hozzám egy ${faszsag}. Mondom neki: "Te ${melleknev}, nem látod hogy épp ${fonev}-t sétáltatok?!". Ezek után elkezdtem magamban dúdolni a ${fonev2} által írt zenét, amelynek a címe: ${melleknev2} ${ige} a vadonban. Olyan szép hangom volt, hogy még a szomszéd is ${multIge} az utcán tőle.`,
+  
+    `Tegnap ${fonev} ${ige} a hídon és összetálkozik egy ${fonev2}-al/el. Mindkettőjük úgy érezte magát, mintha ${melleknev} cseresznyék esnének az égből. ${fonev} ${multIge} egyet majd otthagyta a ${melleknev} ${fonev2}-t mint egy ${faszsag}.`,
+  
+    `A tavalyi nyaramat haverom farmján töltöttem ami ${melleknev} volt. Annyit kell róla tudni, hogy termel búzát, zabot és ${fonev}-t is. Tyúkot is tenyészt, ami ${ige} napokig, majd a tojásból kikel egy kis bolyhos, ${melleknev2} ${fonev2}. Azóta a haverom annyit ${multIge}, hogy már ismét kedvem van elmenni az ő ${faszsag} farmjára.`];
+
+
+    const random = Math.floor((Math.random() * text.length ));
+
+    document.getElementById("text").innerHTML =  text[random];
+}
+
+document.getElementById("flare").addEventListener("click", function(){
+  document.getElementById("audio").volume = 0.5;
+  document.getElementById("audio").play();
+})
